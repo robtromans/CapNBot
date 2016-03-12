ca30f8fbfdfbc76a1385a4f2dfd40b2325531e63
Andrew Lowther <andrew.lowther@mademedia.co.uk>
2016-03-12 18:44:56 +0000

    Adding an IRC class and connection configs.

---
+++ CapNBot/IrcClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CapNBot
+{
+    class IrcClient
+    {
+
+        private TcpClient tcpClient;
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        private string channel;
+        private string username;
+
+        public IrcClient(string ip, int port, string username, string password)
+        {
+            tcpClient = new TcpClient(ip, port);
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+
+            this.username = username;
+
+            writer.WriteLine("PASS " + password);
+            writer.WriteLine("NICK " + username);
+            writer.WriteLine("USER " + username + " 8 * :" + username);
+            writer.Flush();
+        }
+
+        public void joinChannel(string channel)
+        {
+            this.channel = channel;
+
+            writer.WriteLine("JOIN #" + channel);
+            writer.Flush();
+        }
+
+        public void sendIRCMessage(string message)
+        {
+            writer.WriteLine(message);
+            writer.Flush();
+        }
+
+        public void sendChatMessage(string message)
+        {
+            sendIRCMessage(":" + username + "!" + username + "@" + username 
+                + ".tmi.twitch.tv PRIVMSG #" + channel + " :" + message);
+        }
+
+        public string readMessage()
+        {
+            string message = reader.ReadLine();
+            return message;
+        }
+    }
+}
--- CapNBot/Program.cs
+++ CapNBot/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace CapNBot
 {
@@ -10,6 +11,23 @@ namespace CapNBot
     {
         static void Main(string[] args)
         {
+            IrcClient irc = new IrcClient(
+                ConfigurationManager.AppSettings["twitchDefaultIrcPath"],
+                6667,
+                ConfigurationManager.AppSettings["twitchOauthUsername"],
+                ConfigurationManager.AppSettings["twitchOauthPassword"]
+            );
+
+            irc.joinChannel(ConfigurationManager.AppSettings["twitchChannel"]);
+
+            while(true)
+            {
+                string message = irc.readMessage();
+                if (message.Contains("!hello"))
+                {
+                    irc.sendChatMessage("Hey friend!");
+                }
+            }
         }
     }
 }
